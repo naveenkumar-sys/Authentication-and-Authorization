8d328c51e53d7123d79e1144da71c5d4f1cce8de
naveenkumar-sys <naveenrnk69@gmail.com>
2025-12-05 10:06:13 +0530

    completed

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -7,10 +7,11 @@ import {
 import { authMiddleware } from "../Middlewares/AuthMiddleware.js";
 import { adminMiddleware } from "../Middlewares/adminMiddleware.js";
 
+
 const router = express.Router();
 
 router.post("/registerUser", registerUser);
 router.post("/loginUser", loginUser);
-router.get("/getAll", authMiddleware, adminMiddleware, getAllUser);
+router.get("/getAll", authMiddleware,adminMiddleware, getAllUser);
 
 export default router;
